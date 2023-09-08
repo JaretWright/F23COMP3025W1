32b80d8f7eccf801d54cfb86e6f7d6be15bc75b7
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-09-08 10:48:32 -0400

    Added a little logic to the controller

--- app/src/main/java/com/example/f23comp3025w1/MainActivity.kt
+++ app/src/main/java/com/example/f23comp3025w1/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.f23comp3025w1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.f23comp3025w1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //define a variable to access the binding class (makes it easier for us to connect with the id's in the view/layout file)
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)  //connecting the binding variable to the class
+
+        setContentView(binding.root)  //connect the controller class to access the binding class
+
+        binding.button.setOnClickListener {
+            binding.textView.text = "Hello "+binding.nameEditText.text.toString()
+        }
     }
 }
